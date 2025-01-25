888ea9235c91fafeda427c12e35812fb15a9cd58
Gamer <Gamer@DESKTOP-MQB4JFM>
2025-01-25 03:35:34 +0100

    bouncing fixed and consistent

--- Assets/Scripts/BubbleMovement.cs
+++ Assets/Scripts/BubbleMovement.cs
@@ -2,23 +2,46 @@ using UnityEngine;
 
 public class BubbleMovement : MonoBehaviour
 {
-    public Vector3 velocity = new Vector3(0, +1, 0);
-
+    public Vector3 velocity = new Vector3(0, 1, 0);  // Initial velocity
     private Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.useGravity = false;
-    }
-
-    void Update()
-    {
         rb.linearVelocity = velocity;
+
+        IgnoreBubbleCollisions();
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        velocity = -velocity;
+        // hack this bullshit cause fuck it
+        Vector3 dir = rb.linearVelocity.normalized;
+        rb.linearVelocity = dir * 10;
+
+        if (collision.gameObject.CompareTag("Bubble"))
+        {
+            return;
+        }
+
+        if (collision.contacts.Length > 0)
+        {
+            Vector3 normal = collision.contacts[0].normal;
+            velocity = Vector3.Reflect(velocity, normal);
+
+            rb.linearVelocity = velocity.normalized * rb.linearVelocity.magnitude;
+        }
+    }
+
+    void IgnoreBubbleCollisions()
+    {
+        GameObject[] allBubbles = GameObject.FindGameObjectsWithTag("Bubble");
+        foreach (var bubble in allBubbles)
+        {
+            if (bubble != gameObject) // Ignore self-collision
+            {
+                Physics.IgnoreCollision(bubble.GetComponent<Collider>(), GetComponent<Collider>());
+            }
+        }
     }
 }
